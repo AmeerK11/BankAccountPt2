4c00a49dd89a4a02b3b4957eba30f996285d4554
TeejMcSteez <tjhall047@gmail.com>
2025-06-19 21:07:57 -0400

    fix: forgot to add current balance in super init

--- accounts.py
+++ accounts.py
@@ -2,7 +2,7 @@ from bank_account import BankAccount
 
 class Savings(BankAccount):
     def __init__(self, customer_name, account_num, routing_num, current_balance, min_balance, rate):
-        super().__init__(customer_name, account_num, routing_num, current_balance, min_balance)
+        super().__init__(customer_name, current_balance, min_balance, account_num, routing_num)
         self.rate = rate
 
     def incr(self):
