d2cafafaa358f41c483bde29cdf59bde17dd8584
TeejMcSteez <tjhall047@gmail.com>
2025-06-19 21:38:36 -0400

    fix: realized didnt need to implement super methods, +1 for inheritance

--- accounts.py
+++ accounts.py
@@ -8,18 +8,10 @@ class Savings(BankAccount):
     def incr(self):
         intr = self.current_balance * self.rate
         self.deposit(intr)
-    def deposit(self, amount):
-        return super().deposit(amount)
-    def withdraw(self, amount):
-        return super().withdraw(amount)
 class Checking(BankAccount):
     def __init__(self, customer_name, account_num, routing_num, current_balance, min_balance, LIMIT):
         super().__init__(customer_name, current_balance, min_balance, account_num, routing_num)
         self.LIMIT = LIMIT
-    def withdraw(self, amount):
-        return super().withdraw(amount)
-    def deposit(self, amount):
-        return super().deposit(amount)
     def transfer(self, amount, account_num, routing_num):
         if amount <= self.LIMIT and amount > 0:
             print(f"Sending {amount} to account: {account_num}, routing: {routing_num}")
