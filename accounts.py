399ad57eb88c24bd056e78ed9a34ee813294fc60
TeejMcSteez <tjhall047@gmail.com>
2025-06-19 20:40:21 -0400

    feat: removed template files and combined savings and checkings into an accounts module

---
+++ accounts.py
@@ -0,0 +1,18 @@
+from main import BankAccount
+
+class Savings(BankAccount):
+    def __init__(self, acct, routing, balance, rate):
+        super().__init__(acct, routing, balance)
+        self.rate = rate
+
+    def incr(self):
+        self.deposit(self.balance * self.rate)
+class Checking(BankAccount):
+    def __init__(self, acct, routing, balance, LIMIT):
+        super.__init__(acct, routing, balance)
+        self.LIMIT = LIMIT
+    def withdraw(self, amount):
+        if amount <= self.current_balance + self.LIMIT:
+            self.current_balance -= amount
+        else:
+            print("Overdraft limit exceeded")
